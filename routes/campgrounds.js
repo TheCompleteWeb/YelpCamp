4ff017a6c9b614793e3f98cd398a69d858ee9696
neogeoss <neogeo-s@hanmail.net>
2018-10-03 02:44:11 +0000

    only owner of a page sees delete and edit button

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -65,7 +65,7 @@ router.get('/:id/edit', checkCampgroundOwnership, function(req, res){
                 });
 });
 
-router.put('/:id', function(req, res){
+router.put('/:id', checkCampgroundOwnership, function(req, res){
     var data = {name: req.body.name, image:req.body.image}
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
@@ -76,7 +76,7 @@ router.put('/:id', function(req, res){
     });
 });
 
-router.delete('/:id', function(req, res){
+router.delete('/:id', checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
             res.redirect("/campgrounds");
