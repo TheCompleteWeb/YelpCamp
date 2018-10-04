8a626c79e845213fcc298400110eb25afd20c30b
neogeoss <neogeo-s@hanmail.net>
2018-10-04 15:11:06 +0000

    Comment delete and edit bars are added

--- routes/comments.js
+++ routes/comments.js
@@ -69,4 +69,14 @@ router.put('/:comment_id', function(req, res){
     });
 });
 
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 module.exports = router;
